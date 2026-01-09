42612a434ac068d2f5c129a527933ac90957357b
joel <joelsantossouza08@gmail.com>
2026-01-09 13:34:32 +0000

    Decide the simplest way to put log messages, just passing as parameter the mutex and creating a function time_elapsed() to get the timestamp

--- srcs/coder_routine/log_message.c
+++ srcs/coder_routine/log_message.c
@@ -6,12 +6,17 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 12:40:53 by joesanto          #+#    #+#             */
-/*   Updated: 2026/01/09 13:07:21 by joesanto         ###   ########.fr       */
+/*   Updated: 2026/01/09 13:33:25 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "codexion.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
 
-void	log_message(uint64_t timestamp_in_ms, uint32_t coder_id, const char *msg)
+void	log_message(uint64_t timestamp_in_ms, uint32_t coder_id, const char *msg, pthread_mutex_t *mutex)
 {
+	pthread_mutex_lock(mutex);
+	printf("%lu %d %s\n", timestamp_in_ms, coder_id, msg);
+	pthread_mutex_unlock(mutex);
 }
