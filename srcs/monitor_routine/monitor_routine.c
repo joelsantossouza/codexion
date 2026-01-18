2a2c1e178a2e89c17b652a886e8030d470917172
joel <joelsantossouza08@gmail.com>
2026-01-18 21:44:27 +0000

    Starting to write the monitoring routine thread

--- srcs/monitor_routine/monitor_routine.c
+++ srcs/monitor_routine/monitor_routine.c
@@ -6,7 +6,7 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:37:06 by joesanto          #+#    #+#             */
-/*   Updated: 2026/01/18 19:53:32 by joesanto         ###   ########.fr       */
+/*   Updated: 2026/01/18 21:43:02 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,20 @@ void	*start_monitoring(t_coder coders[])
 	while (TRUE)
 	{
 		
-		if (scheduler())
-		if (scheduler(&available_dongles, ncoders, coders, dongle_cooldown) >= ncompiles_required)
-			break ;
+		if (scheduler(coders, config, &available_dongles, &priority_coder) == STOP_SIMULATION)
+		{
+			// TODO: set all coder as dead, then print the priority_coder that die
+		}
+		pthread_mutex_lock(&priority_coder->local_mutex);
+		if (priority_coder->state & WAITING_TO_COMPILE)
+		{
+			while (!(priority_coder->state & TWO_DONGLES) && available_dongles > 0)
+			{
+				available_dongles--;
+				priority_coder->state += ONE_DONGLE;
+				mutex_log_msg(time_elapsed(0), priority_coder->id, TAKEN_DONGLE_MSG, priority_coder->global_mutex);
+			}
+		}
+		pthread_mutex_unlock(&priority_coder->local_mutex);
 	}
-	return (0);
 }
