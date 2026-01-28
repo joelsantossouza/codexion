0f5ec14d3d50b002c01dfedf02a28b3cccd57de2
joel <joelsantossouza08@gmail.com>
2026-01-28 22:59:42 +0000

    Improving performance and security of the log_coder_event by passing events_id intead of messages to guarantee that the log event will just be the existing ones, and getting safe of bigger event_msgs strings than LOG_BUFFER_SIZE

--- srcs/coder_routine/log_coder_event.c
+++ srcs/coder_routine/log_coder_event.c
@@ -6,13 +6,12 @@
 /*   By: joesanto <joesanto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 16:51:51 by joesanto          #+#    #+#             */
-/*   Updated: 2026/01/28 22:33:27 by joesanto         ###   ########.fr       */
+/*   Updated: 2026/01/28 22:58:12 by joesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdint.h>
-#include <string.h>
 #include "coder_internal.h"
 
 static inline
@@ -65,20 +64,34 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-void	log_coder_event(t_coder *coder, const char *event_msg)
+int	log_coder_event(t_coder *coder, enum e_event_id event_id)
 {
-	const uint32_t	event_msg_len = strlen(event_msg);
 	char			log[LOG_BUFFER_SIZE];
 	uint32_t		log_len;
+	static const char	*events_msg[MAX_EVENTS] = {
+		MSG_TAKEN_DONGLE,
+		MSG_COMPILING,
+		MSG_DEBUGGING,
+		MSG_REFACTORING,
+		MSG_BURNOUT
+	};
+	static const uint32_t	events_msg_len[MAX_EVENTS] = {
+		MSG_TAKEN_DONGLE_STRLEN,
+		MSG_COMPILING_STRLEN,
+		MSG_DEBUGGING_STRLEN,
+		MSG_REFACTORING_STRLEN,
+		MSG_BURNOUT_STRLEN
+	};
 
-	if (is_simulation_running() == false)
-		return ;
+	if (event_id >= MAX_EVENTS || is_simulation_running() == false)
+		return (-1);
 	log_len = buffer_ultoa(timestamp_ms(0), log);
 	log_len += buffer_ultoa(coder->id, log + log_len);
-	ft_memcpy(log + log_len, event_msg, event_msg_len);
-	log_len += event_msg_len;
+	ft_memcpy(log + log_len, events_msg[event_id], events_msg_len[event_id]);
+	log_len += events_msg_len[event_id];
 	log[log_len++] = '\n';
 	pthread_mutex_lock();
 	write(STDOUT_FILENO, log, log_len);
 	pthread_mutex_unlock();
+	return (log_len);
 }
